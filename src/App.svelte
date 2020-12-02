34870de8b2285c63c351a0d752d24d831365bb1c
Saikat Das <saikatdas0790@gmail.com>
2020-12-02 08:47:04 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+</script>
+
+<style>
+  .App-header {
+    background-color: #f9f6f6;
+    color: #333;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+  }
+
+  .App-logo {
+    height: 36vmin;
+    pointer-events: none;
+    margin-bottom: 3rem;
+    animation: App-logo-pulse infinite 1.6s ease-in-out alternate;
+  }
+
+  @keyframes App-logo-pulse {
+    from {
+      transform: scale(1);
+    }
+    to {
+      transform: scale(1.06);
+    }
+  }
+</style>
+
+<div class="App">
+  <header class="App-header">
+    <img src="/logo.svg" alt="logo" class="App-logo" />
+    <a
+      href="https://svelte.dev"
+      class="App-link"
+      target="_blank"
+      rel="noopener noreferrer">Learn Svelte</a>
+  </header>
+</div>
